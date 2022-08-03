17975779f5a1061ee0a4a4b14ad977b91ec24e82
ryllerpadua <ryllerpadua@yahoo.com.br>
2022-08-03 10:57:39 -0300

    Start CRUD

---
+++ db/migrate/20220803133844_create_clients.rb
@@ -0,0 +1,11 @@
+class CreateClients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :clients do |t|
+      t.text :name
+      t.string :cpf
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
